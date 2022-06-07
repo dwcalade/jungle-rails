d361b5a62e3770303affd156b149760df8dd8bae
dwcalade <dwcalade@yahoo.ca>
2022-06-07 04:47:05 +0000

    Fix Routes/Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to users_new
+      redirect_to signup_path
     end
   end
 
