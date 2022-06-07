a00917dc0e37f9057661d5d0fe83e74d7bae7873
dwcalade <dwcalade@yahoo.ca>
2022-06-07 19:21:50 +0000

    Adding error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
-
 RSpec.describe Product, type: :model do
-
+  
   before(:all) do
     @category = Category.find_or_create_by name: 'Trees'
     @product = @category.products.create({
@@ -9,26 +8,36 @@ RSpec.describe Product, type: :model do
       description: "test",
       image: 'plante_1.jpg',
       quantity: 0,
-      price: 64.99
-      })
-    end
-
+      price_cents: 64.99
+      })    
+  end
+  
   describe 'Validations' do
     it 'returns true Product includes name, price, quantity & category' do
-      expect(@product.name).to be_truthy
-      expect(@product.price).to be_truthy
-      expect(@product.quantity).to be_truthy
-      expect(@product.category).to be_truthy
+      expect(@product.valid?).to be_truthy
     end
-    it 'returns true Product includes name' do
-      expect(@product.name).to be_truthy
+
+    it 'returns nil when Product does not include name' do
+      @product.name = nil
+      @product.save
+      expect(@product.name).to be(nil)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
-    it 'returns true Product includes price' do
-      expect(@product.price).to be_truthy
+
+    it 'returns nil when Product does not include price' do
+      @product.price_cents = nil
+      @product.save
+      expect(@product.price_cents).to be(nil)
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
-    it 'returns true Product includes quantity' do
-      expect(@product.quantity).to be_truthy
+
+    it 'returns nil when Product does not include quantity' do
+      @product.quantity = nil
+      @product.save
+      expect(@product.quantity).to be(nil)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
+
     it 'returns true Product includes category' do
       expect(@product.category).to be_truthy
     end
