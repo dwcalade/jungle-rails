2f1e77156c07f91413d492081ba6dbe82e73bb4c
dwcalade <dwcalade@yahoo.ca>
2022-06-09 06:36:28 +0000

    Prepping App2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+
   def new
   end
+
   def create
       user = User.new(user_params)
     if user.save
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:first, :last, :email, :password, :password_confirmation)
     end
